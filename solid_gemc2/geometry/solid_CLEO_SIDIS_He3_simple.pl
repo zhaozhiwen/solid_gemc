5fb233374e913c74815635beec5c963541759388
zwzhao <zwzhao@d678033d-4291-0410-8c47-e848bf2bee03>
2014-06-05 16:30:47 +0000

    test solid geometry for GEMC2

---
+++ solid_gemc2/geometry/solid_CLEO_SIDIS_He3_simple.pl
@@ -0,0 +1,82 @@
+#!/usr/bin/perl -w
+
+use strict;
+use lib ("$ENV{GEMC}/io");
+use parameters;
+use utils;
+
+use geometry;
+use hit;
+use bank;
+use math;
+
+use Math::Trig;
+# use Math::MatrixReal;
+# use Math::VectorReal;
+
+# system("rm meic_det1_simple__*txt");
+
+# Help Message
+sub help()
+{
+	print "\n Usage: \n";
+	print "   detector.pl <configuration filename>\n";
+ 	print "   Will create the detector\n";
+ 	print "   Note: The passport and .visa files must be present to connect to MYSQL. \n\n";
+	exit;
+}
+
+# Make sure the argument list is correct
+if( scalar @ARGV != 1) 
+{
+	help();
+	exit;
+}
+
+
+# Loading configuration file and paramters
+my $config_file   = $ARGV[0];
+our %configuration = load_configuration($config_file);
+
+#if($#ARGV < 0){
+#    do 'configs/pvdis_CLEO_nominal.pl';
+#}else{
+#    do 'configs/'.$ARGV[0];
+#}
+
+# One can change the "variation" here if one is desired different from the config.dat
+$configuration{"detector_name"} = "solid_CLEO_SIDIS_He3_simple";
+$configuration{"variation"} = "Original";
+
+# To get the parameters proper authentication is needed.
+# our %parameters    = get_parameters(%configuration);
+# $configuration{"detector_name"} = "solid_CLEO_SIDIS";
+
+#Geometry definition
+require "solid_CLEO_solenoid_kill.pl";
+solid_CLEO_solenoid_kill();
+require "solid_CLEO_SIDIS_target_He3_collimator_kill.pl";
+solid_CLEO_SIDIS_target_He3_collimator_kill();
+require "solid_CLEO_SIDIS_ec_forwardangle_kill.pl";
+solid_CLEO_SIDIS_ec_forwardangle_kill();
+require "solid_CLEO_SIDIS_ec_largeangle_kill.pl";
+solid_CLEO_SIDIS_ec_largeangle_kill();
+require "solid_CLEO_SIDIS_virtualplane_cher.pl";
+solid_CLEO_SIDIS_virtualplane_cher();
+require "solid_CLEO_SIDIS_virtualplane_ec.pl";
+solid_CLEO_SIDIS_virtualplane_ec();
+require "solid_CLEO_SIDIS_virtualplane_gem.pl";
+solid_CLEO_SIDIS_virtualplane_gem();
+require "solid_CLEO_SIDIS_virtualplane_mrpc.pl";
+solid_CLEO_SIDIS_virtualplane_mrpc();
+require "solid_CLEO_SIDIS_virtualplane_spd.pl";
+solid_CLEO_SIDIS_virtualplane_spd();
+
+# Hit definition
+# Execute only when there are changes
+# require "./meic_det1_hit.pl";
+# flux_hit();
+
+# banks
+# require "./meic_det1_bank.pl";
+# define_flux_bank();
---
+++ solid_gemc2/geometry/solid_CLEO_SIDIS_ec_forwardangle.pl
@@ -0,0 +1,103 @@
+use strict;
+use warnings;
+our %detector;
+our %configuration;
+our %parameters;
+
+use Getopt::Long;
+use Math::Trig;
+
+my $DetectorName = 'solid_CLEO_SIDIS_ec_forwardangle';
+
+my $DetectorMother="root";
+
+sub solid_CLEO_SIDIS_ec_forwardangle
+{
+make_ec_forwardangle();
+make_ec_forwardangle_shield();
+}
+
+# HOD2SLATS       2
+# HOD2MEDIUM    654
+# HOD2MOTHER   'HALL' 
+# HOD2IDTYPE       42
+# HOD2GATE       80.
+# HOD2THRES       0
+# HOD2SHAP      'CONE' 'CONE'
+# HOD2SIZE1      9.0  108.  197. 111. 202.  
+# # HOD2SIZE2      2.0  107.00  196. 108. 197.  
+# HOD2TYPE        1 2
+# HOD2POSX        2*0.
+# HOD2POSY        2*0.
+# HOD2POSZ        740. 729. 
+
+ my $color="0000ff";
+ my $material="LgTF1";
+ my $material_shield="Lead";
+
+sub make_ec_forwardangle
+{
+#  my $z=775-350;
+#   my $z=765-350;
+#   my $z=430;
+  my $z=440;
+
+    my %detector=init_det();
+    $detector{"name"}        = "$DetectorName";
+    $detector{"mother"}      = "$DetectorMother" ;
+    $detector{"description"} = $detector{"name"};
+    $detector{"pos"}        = "0*cm 0*cm $z*cm";
+    $detector{"rotation"}   = "0*deg 0*deg 0*deg";
+    $detector{"color"}      = $color;
+    $detector{"type"}       = "Cons";
+    my $Rmin1 = 98;
+    my $Rmax1 = 230;
+    my $Rmin2 = 98;
+    my $Rmax2 = 230;
+    my $Dz    = 25;
+    my $Sphi  = 0;
+    my $Dphi  = 360;
+    $detector{"dimensions"}  = "$Rmin1*cm $Rmax1*cm $Rmin2*cm $Rmax2*cm $Dz*cm $Sphi*deg $Dphi*deg";
+    $detector{"material"}   = "$material";
+    $detector{"mfield"}     = "no";
+    $detector{"ncopy"}      = 1;
+    $detector{"pMany"}       = 1;
+    $detector{"exist"}       = 1;
+    $detector{"visible"}     = 1;
+    $detector{"style"}       = 1;
+    $detector{"sensitivity"} = "no";
+    $detector{"hit_type"}    = "no";
+    $detector{"identifiers"} = "no";
+    print_det(\%configuration, \%detector);
+}
+
+sub make_ec_forwardangle_shield
+{
+  my %detector=init_det();
+ $detector{"name"}        = "$DetectorName\_shield";
+ $detector{"mother"}      = "root";
+ $detector{"description"} = $detector{"name"};
+ $detector{"pos"}         = "0*cm 0*cm 413.8*cm";
+ $detector{"rotation"}    = "0*deg 0*deg 0*deg";
+ $detector{"color"}       = $color;
+ $detector{"type"}        = "Cons";
+  my $Rmin1 = 98;
+  my $Rmax1 = 230;
+  my $Rmin2 = 98;
+  my $Rmax2 = 230;
+  my $Dz    = 0.56;
+  my $Sphi  = 0;
+  my $Dphi  = 360;
+ $detector{"dimensions"}  = "$Rmin1*cm $Rmax1*cm $Rmin2*cm $Rmax2*cm $Dz*cm $Sphi*deg $Dphi*deg";
+ $detector{"material"}    = $material_shield;
+ $detector{"mfield"}      = "no";
+ $detector{"ncopy"}       = 1;
+ $detector{"pMany"}       = 1;
+ $detector{"exist"}       = 1;
+ $detector{"visible"}     = 1;
+ $detector{"style"}       = 0;
+ $detector{"sensitivity"} = "no";
+ $detector{"hit_type"}    = "no";
+ $detector{"identifiers"} = "no";
+ print_det(\%configuration, \%detector);
+}
