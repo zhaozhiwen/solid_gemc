3d0d5d22cd5072b3f481ad60edf62777f8357abf
zwzhao <zwzhao@d678033d-4291-0410-8c47-e848bf2bee03>
2014-07-18 01:29:27 +0000

    add more hit process

--- solid_gemc2/source/hitprocess/solid_hitprocess.h
+++ solid_gemc2/source/hitprocess/solid_hitprocess.h
@@ -6,13 +6,16 @@
 
 #include "solidec_hitprocess.h"
 #include "solidgem_hitprocess.h"
-// #include "solid_gem_trace_hitprocess.h"
+#include "solidcc_hitprocess.h"
+#include "solidmrpc_hitprocess.h"
 
 //   This function allows us to add in our own hit processor for SoLID
 
 void solid_hitprocess( map<string, HitProcess_Factory> &hitMap ){
     hitMap["solidec"]  = &solidec_HitProcess::createHitClass;    
-    hitMap["solidgem"]  = &solidgem_HitProcess::createHitClass;
+    hitMap["solidgem"] = &solidgem_HitProcess::createHitClass;
+    hitMap["solidcc"]  = &solidcc_HitProcess::createHitClass;    
+    hitMap["solidmrpc"]= &solidmrpc_HitProcess::createHitClass;        
 //     hitMap["solid_gem_trace"]  = &solid_gem_trace_HitProcess::createHitClass;    
     return;
 }
