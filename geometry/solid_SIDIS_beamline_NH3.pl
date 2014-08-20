b2e275d88b53f3b6b1062a622a62e71a62dc0acc
zwzhao <zwzhao@d678033d-4291-0410-8c47-e848bf2bee03>
2014-08-20 00:24:12 +0000

    update  NH3 target with chmaber,windows,beamline

--- geometry/solid_SIDIS_beamline_NH3.pl
+++ geometry/solid_SIDIS_beamline_NH3.pl
@@ -13,7 +13,7 @@ my $DetectorMother="root";
 
 sub solid_SIDIS_beamline_NH3
 {
-make_beam_entrance();
+# make_beam_entrance();
 make_beam_exit();
 }
 
@@ -77,15 +77,16 @@ sub make_beam_exit
 #  my @Rmax2 = (40.,39.,38.,1.7);
 #  my @Dz   = (310.,310.,1.,0.0125);
 # to avoid overlap with downsteam yoke
- my @z    = (315.-350,0.,290,-290);
- my @Rmin1  = (0.,0.,0.,0.);
- my @Rmax1 = (1.80,1.70,35.0,1.7);
- my @Rmin2  = (0.,0.,0.,0.);
- my @Rmax2 = (37.,36.,35.,1.7);
- my @Dz   = (290.,290.,1.,0.0125);
+#  my @z    = (-325+412.5,0.,412.4,-412.4875);
+#  my @Rmin1  = (0.,0.,0.,0.);
+#  my @Rmax1 = (1.80,1.70,28.9,1.7);
+#  my @Rmin2  = (0.,0.,0.,0.);
+#  my @Rmax2 = (29.,28.9,28.9,1.7);
+#  my @Dz   = (412.5,412.5,0.1,0.0125);
  my @name = ("B3PP","B3PV","B3DM","B3W1"); 
  my @mother=("$DetectorMother","$DetectorName\_B3PP","$DetectorName\_B3PV","$DetectorName\_B3PV");
- my @mat  = ("G4_Al","SL_Vacuum","Kryptonite","G4_Be");
+ my @mat  = ("G4_Al","SL_Vacuum","SL_Vacuum","G4_Be");
+ my @color = ("0000ff","808080","808080","00FFFF");
 
  for(my $n=1; $n<=$NUM; $n++)
  {
@@ -94,14 +95,20 @@ sub make_beam_exit
     $detector{"name"}        = "$DetectorName\_$name[$n-1]";
     $detector{"mother"}      = "$mother[$n-1]" ;
     $detector{"description"} = $detector{"name"};
-    $detector{"pos"}        = "0*cm 0*cm $z[$n-1]*cm";
+#     $detector{"pos"}        = "0*cm 0*cm $z[$n-1]*cm";
+    $detector{"pos"}        = "0*cm 0*cm 0*cm";
     $detector{"rotation"}   = "0*deg 0*deg 0*deg";
-    $detector{"color"}      = "ff6633";
-    $detector{"type"}       = "Cons";
-    $detector{"dimensions"} = "$Rmin1[$n-1]*cm $Rmax1[$n-1]*cm $Rmin2[$n-1]*cm $Rmax2[$n-1]*cm $Dz[$n-1]*cm 0*deg 360*deg";
+    $detector{"color"}      = $color[$n-1];
+#     $detector{"type"}       = "Cons";
+#     $detector{"dimensions"} = "$Rmin1[$n-1]*cm $Rmax1[$n-1]*cm $Rmin2[$n-1]*cm $Rmax2[$n-1]*cm $Dz[$n-1]*cm 0*deg 360*deg";
+    $detector{"type"}       = "Polycone";
+    if ($n==1) {$detector{"dimensions"} = "0*deg 360*deg 3*counts 0*cm 0*cm 0*cm 1.05*cm 19*cm 19*cm -325*cm 190*cm 550*cm";}
+    if ($n==2) {$detector{"dimensions"} = "0*deg 360*deg 3*counts 0*cm 0*cm 0*cm 1.*cm 18.95*cm 18.95*cm -325*cm 190*cm 550*cm";}
+    if ($n==3) {$detector{"dimensions"} = "0*deg 360*deg 2*counts 0*cm 0*cm 18.95*cm 18.95*cm 549.9*cm 550*cm";}
+    if ($n==4) {$detector{"dimensions"} = "0*deg 360*deg 2*counts 0*cm 0*cm 1.*cm 1.*cm -325*cm -324.99*cm";}
     $detector{"material"}   = $mat[$n-1];
     $detector{"mfield"}     = "no";
-    $detector{"ncopy"}      = 1;
+    $detector{"ncopy"}      = $n;
     $detector{"pMany"}       = 1;
     $detector{"exist"}       = 1;
     $detector{"visible"}     = 1;
@@ -109,6 +116,7 @@ sub make_beam_exit
     $detector{"sensitivity"} = "no";
     $detector{"hit_type"}    = "no";
     $detector{"identifiers"} = "no";
+
     print_det(\%configuration, \%detector);
  }
 }
