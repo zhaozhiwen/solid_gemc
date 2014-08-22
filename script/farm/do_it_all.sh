af037c6d8a2e43568198fcb2cc69a4bd94eb133e
zwzhao <zwzhao@d678033d-4291-0410-8c47-e848bf2bee03>
2014-08-22 18:11:10 +0000

    minor edit

--- script/farm/do_it_all.sh
+++ script/farm/do_it_all.sh
@@ -1,14 +1,17 @@
 #!/bin/csh
 uname -a | cat > out.log
+more /etc/redhat-release | cat >> out.log
 more /proc/cpuinfo | cat >> out.log
 more /proc/meminfo | cat >> out.log
 
 #use official installation on farm and ifarm
 source /home/zwzhao/set_solid 
 
+#run simulation
 solid_gemc solid.gcard | cat >> out.log
 
-#need more option for banks more than flux
+#convert evio to root
+#need more option for banks other than flux
 evio2root -INPUTF=out.evio | cat >> out.log
 
 
