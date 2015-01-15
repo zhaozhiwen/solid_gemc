79a4c0c0d70685fef599311c8da132d7613f86b2
zwzhao <zwzhao@d678033d-4291-0410-8c47-e848bf2bee03>
2015-01-15 16:00:38 +0000

    fix the bug that  absorber is not Krypotnite

--- solid_gemc2/geometry/solid_JPsi_simple.pl
+++ solid_gemc2/geometry/solid_JPsi_simple.pl
@@ -55,8 +55,8 @@ $configuration{"variation"} = "Original";
 #Geometry definition
 require "solid_solenoid_kill.pl";
 solid_solenoid_kill();
-require "solid_SIDIS_absorber.pl";
-solid_SIDIS_absorber();
+require "solid_SIDIS_absorber_kill.pl";
+solid_SIDIS_absorber_kill();
 require "solid_SIDIS_ec_forwardangle_kill.pl";
 solid_SIDIS_ec_forwardangle_kill();
 require "solid_SIDIS_ec_largeangle_kill.pl";
