d0c8eebc0fa3c39850e2969bdff9eabbcf7b724c
zwzhao <zwzhao@d678033d-4291-0410-8c47-e848bf2bee03>
2015-11-17 16:05:13 +0000

    making hit process consistant

--- source/2.2/hitprocess/solid_hitprocess.h
+++ source/2.2/hitprocess/solid_hitprocess.h
@@ -7,6 +7,8 @@
 #include "solid_ec_hitprocess.h"
 #include "solid_gem_hitprocess.h"
 #include "solid_hgc_hitprocess.h"
+#include "solid_lgc_hitprocess.h"
+#include "solid_spd_hitprocess.h"
 #include "solid_mrpc_hitprocess.h"
 
 //   This function allows us to add in our own hit processor for SoLID
@@ -15,8 +17,9 @@ void solid_hitprocess( map<string, HitProcess_Factory> &hitMap ){
     hitMap["solid_ec"]  = &solid_ec_HitProcess::createHitClass;    
     hitMap["solid_gem"] = &solid_gem_HitProcess::createHitClass;
     hitMap["solid_hgc"]  = &solid_hgc_HitProcess::createHitClass;    
+    hitMap["solid_lgc"]= &solid_lgc_HitProcess::createHitClass;        
+    hitMap["solid_spd"]= &solid_spd_HitProcess::createHitClass;            
     hitMap["solid_mrpc"]= &solid_mrpc_HitProcess::createHitClass;        
-//     hitMap["solid_gem_trace"]  = &solid_gem_trace_HitProcess::createHitClass;    
     return;
 }
 
