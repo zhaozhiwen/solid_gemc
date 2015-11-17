99e22df7a0348c3ca5fecda907f5cc201d0d17e6
zwzhao <zwzhao@d678033d-4291-0410-8c47-e848bf2bee03>
2015-11-17 16:45:25 +0000

    add missing file

---
+++ solid_gemc2/geometry/lgc/solid_SIDIS_lgc_virtualplane.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+our %detector;
+our %configuration;
+our %parameters;
+
+use Getopt::Long;
+use Math::Trig;
+
+my $DetectorName = 'solid_SIDIS_lgc_virtualplane';
+
+my $DetectorMother="root";
+
+sub solid_SIDIS_lgc_virtualplane
+{
+make_lg_front();
+}
+
+sub make_lg_front
+{
+    my %detector=init_det();
+ $detector{"name"}        = "$DetectorName\_front";
+ $detector{"mother"}      = "root";
+ $detector{"description"} = $detector{"name"};
+ $detector{"pos"}         = "0*cm 0*cm 96*cm";
+ $detector{"rotation"}    = "0*deg 0*deg 0*deg";
+ $detector{"color"}       = "CC6633";
+ $detector{"type"}        = "Cons";
+  my $Rmin1 = 58;
+  my $Rmax1 = 127;
+  my $Rmin2 = 58;
+  my $Rmax2 = 127;
+  my $Dz    = 0.001/2;
+  my $Sphi  = 0;
+  my $Dphi  = 360;
+ $detector{"dimensions"}  = "$Rmin1*cm $Rmax1*cm $Rmin2*cm $Rmax2*cm $Dz*cm $Sphi*deg $Dphi*deg";
+ $detector{"material"}    = "G4_Galactic";
+ $detector{"mfield"}      = "no";
+ $detector{"ncopy"}       = 1;
+ $detector{"pMany"}       = 1;
+ $detector{"exist"}       = 1;
+ $detector{"visible"}     = 1;
+ $detector{"style"}       = 0;
+ $detector{"sensitivity"} = "flux";
+ $detector{"hit_type"}    = "flux";
+ $detector{"identifiers"} = "id manual 2110000";
+ print_det(\%configuration, \%detector);
+}
---
+++ solid_gemc2/geometry/spd/solid_SIDIS_spd_virtualplane_forwardangle.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+our %detector;
+our %configuration;
+our %parameters;
+
+use Getopt::Long;
+use Math::Trig;
+
+my $DetectorName = 'solid_SIDIS_spd_virtualplane';
+
+my $DetectorMother="root";
+
+sub solid_SIDIS_spd_virtualplane_forwardangle
+{
+make_solid_SIDIS_spd_virtualplane_forwardangle_front();
+}
+
+sub make_solid_SIDIS_spd_virtualplane_forwardangle_front
+{
+ my %detector=init_det();
+ $detector{"name"}        = "$DetectorName\_front";
+ $detector{"mother"}      = "root";
+ $detector{"description"} = $detector{"name"};
+ $detector{"pos"}         = "0*cm 0*cm 406.5*cm";
+ $detector{"rotation"}    = "0*deg 0*deg 0*deg";
+ $detector{"color"}       = "CC6633";
+ $detector{"type"}        = "Cons";
+  my $Rmin1 = 96;
+  my $Rmax1 = 210;
+  my $Rmin2 = 96;
+  my $Rmax2 = 210;
+  my $Dz    = 0.001/2;
+  my $Sphi  = 0;
+  my $Dphi  = 360;
+ $detector{"dimensions"}  = "$Rmin1*cm $Rmax1*cm $Rmin2*cm $Rmax2*cm $Dz*cm $Sphi*deg $Dphi*deg";
+ $detector{"material"}    = "G4_Galactic";
+ $detector{"mfield"}      = "no";
+ $detector{"ncopy"}       = 1;
+ $detector{"pMany"}       = 1;
+ $detector{"exist"}       = 1;
+ $detector{"visible"}     = 1;
+ $detector{"style"}       = 0;
+ $detector{"sensitivity"} = "flux";
+ $detector{"hit_type"}    = "flux";
+ $detector{"identifiers"} = "id manual 5110000";
+ print_det(\%configuration, \%detector);
+}
