73982db347b83c6bca4436a584cd5da89944c948
zwzhao <zwzhao@d678033d-4291-0410-8c47-e848bf2bee03>
2016-04-29 15:11:36 +0000

    add another plane at large angle

--- geometry/muon/solid_DDVCS_muon_largeangle_virtualplane.pl
+++ geometry/muon/solid_DDVCS_muon_largeangle_virtualplane.pl
@@ -14,10 +14,42 @@ my $DetectorMother="root";
 
 sub solid_DDVCS_muon_largeangle_virtualplane
 {
+make_solid_DDVCS_muon_largeangle_virtualplane_endcapdonut_0();
 make_solid_DDVCS_muon_largeangle_virtualplane_endcapdonut_1();
 make_solid_DDVCS_muon_largeangle_virtualplane_endcapdonut_2();
 make_solid_DDVCS_muon_largeangle_virtualplane_barrel_1();
 make_solid_DDVCS_muon_largeangle_virtualplane_barrel_2();
+}
+
+sub make_solid_DDVCS_muon_largeangle_virtualplane_endcapdonut_0
+{
+ my %detector=init_det();
+ $detector{"name"}        = "$DetectorName\_0";
+ $detector{"mother"}      = "root";
+ $detector{"description"} = $detector{"name"};
+ $detector{"pos"}         = "0*cm 0*cm 209.005*cm";
+ $detector{"rotation"}    = "0*deg 0*deg 0*deg";
+ $detector{"color"}       = "000000";
+ $detector{"type"}        = "Cons";
+  my $Rmin1 = 156;
+  my $Rmax1 = 270;
+  my $Rmin2 = 156;
+  my $Rmax2 = 270;
+  my $Dz    = 0.001/2;
+  my $Sphi  = 0;
+  my $Dphi  = 360;
+ $detector{"dimensions"}  = "$Rmin1*cm $Rmax1*cm $Rmin2*cm $Rmax2*cm $Dz*cm $Sphi*deg $Dphi*deg";
+ $detector{"material"}    = "G4_Galactic";
+ $detector{"mfield"}      = "no";
+ $detector{"ncopy"}       = 1;
+ $detector{"pMany"}       = 1;
+ $detector{"exist"}       = 1;
+ $detector{"visible"}     = 1;
+ $detector{"style"}       = 1;
+ $detector{"sensitivity"} = "flux";
+ $detector{"hit_type"}    = "flux";
+ $detector{"identifiers"} = "id manual 6100000";
+ print_det(\%configuration, \%detector);
 }
 
 sub make_solid_DDVCS_muon_largeangle_virtualplane_endcapdonut_1
